f13bd66172b8f12aa0e20ed23c8e20366acc8016
Isaiah <isaiahjesusmartelmartin@gmail.com>
2021-12-11 02:04:40 +0100

    Frontend almost finished (without login)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,8 @@
 
 import { Component } from '@angular/core';
-
+import { ConfigurationPage } from './configuration/configuration.page';
+import { HomePage } from './home/home.page';
+import { ProjectsPage } from './projects/projects.page';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -10,8 +12,33 @@ export class AppComponent   {
 
   constructor() { }
 
+//   private rootPage: any;
+//   title = 'Inicio'
+//   icon = 'home-music.svg';
+//   pages = {home: {
+//     component: HomePage,
+//     title: 'Inicio',
+//     icon: 'home-music.svg'
+//  },
+//  projects: {
+//   component: ProjectsPage,
+//   title: 'Proyectos',
+//   icon: 'projects-icon.svg'
+// },
+// configuration: {
+//   component: ConfigurationPage,
+//   title: 'Configuración',
+//   icon: 'configuration-icon.svg'
+// }
+    
+//   }
+  
+//  changeNameAndIcon(page){
+//     this.rootPage = this.pages[page].component;
+//     this.title = this.pages[page].title;
+//      this.icon = this.pages[page].icon;
 
-
+//   }
   
 
 
--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -13,31 +13,8 @@ import { MenuController } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(private router: Router, private menu: MenuController) { }
-  calendarGo() {
-    this.router.navigate(['calendar']);
+  constructor() { }
 
-  }
-
-  testsGo() {
-    this.router.navigate(['tests']);
-
-  }
-
-  openFirst() {
-    this.menu.enable(true, 'first');
-    this.menu.open('first');
-  }
-
-  openEnd() {
-    this.menu.open('end');
-  }
-
-  openCustom() {
-    this.menu.enable(true, 'custom');
-    this.menu.open('custom');
-  }
-  
 }
 
 
